1398da9e9d2d6ef5b6bc14ae87c7eb96540ffd5a
David Dovo <david.pham.do@gmail.com>
2026-01-12 00:02:21 -0800

    Implemented auto-tiling algorithm
    Clicking "T" now parses the current tilemap and ensures that the proper tiles variants are used with auto-tiling in accordance to neighboring tiles and their orientations.

--- scripts/tilemaps.py
+++ scripts/tilemaps.py
@@ -1,10 +1,23 @@
 import json
-
 import pygame
 
+# Rules for neighboring tiles and autotiling
+AUTOTILE_MAP = {
+    tuple(sorted([(1, 0), (0, 1)])) : 0,
+    tuple(sorted([(1, 0), (0, 1), (-1, 0)])) : 1,
+    tuple(sorted([(-1, 0), (0, 1)])) : 2,
+    tuple(sorted([(-1, 0), (0, -1), (0, 1)])) : 3,
+    tuple(sorted([(-1, 0), (0, -1)])) : 4,
+    tuple(sorted([(-1, 0), (0, -1), (1, 0)])) : 5,
+    tuple(sorted([(1, 0), (0, -1)])) : 6,
+    tuple(sorted([(1, 0), (0, -1), (0, 1)])) : 7,
+    tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])) : 8,
+}
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)] # nine neighbor tiles
 # List of things we want to apply physics upon
 PHYSICS_TILES = {'grass', 'stone'}
+# Auto-tiling assets
+AUTOTILE_TYPES = {'grass', 'stone'}
 
 class Tilemap:
     def __init__(self, game, tile_size=16):
@@ -49,6 +62,19 @@ class Tilemap:
             if tile['type'] in PHYSICS_TILES:
                 rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
         return rects
+
+    def autotile(self):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            neighbors = set()
+            for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                check_loc = str(tile['pos'][0] + shift[0]) + ';' + str(tile['pos'][1] + shift[1])
+                if check_loc in self.tilemap:
+                    if self.tilemap[check_loc]['type'] == tile['type']:
+                        neighbors.add(shift)
+            neighbors = tuple(sorted(neighbors))
+            if tile['type'] in AUTOTILE_TYPES and neighbors in AUTOTILE_MAP:
+                tile['variant'] = AUTOTILE_MAP[neighbors]
 
     def render(self, surface, offset=(0, 0)):
         # Render the off-grid "decorative elements" first
